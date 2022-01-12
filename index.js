b8b8dd221a98ed99753fd84da18f4771305ca923
howsilence <statton0104@gmail.com>
2022-01-12 11:01:48 -0500

    commit tree e67e7bcdd90c0d2bc1d8759e13af46e7c9c8b455

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // code your solution here
+const records = [
+    { year: "2018", result: "N/A"},
+    { year: "2017", result: "N/A"},
+    { year: "2016", result: "N/A"},
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"}]
+// function isWin(record){
+//   return record.result === "W"
+// };
+
+const superbowlWin = records => {
+  const found = records.find((element)=>{
+    return element.result === "W"
+    })    
+  return found ? found.year : undefined
+  }
+// const finder = records.find((element) => {return result === "W"})
+// function superbowlWin(){
+//   return finder.year
+// }
